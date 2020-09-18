acd4795c9f07027bce1c3454bf4993e810d49e59
kelibst <kboster17@gmail.com>
2020-09-18 04:11:07 +0000

    Updates

---
+++ precache-manifest.0494eef84de1aefe507d40ab67154b68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c76513c5a04033ef9453ee4c1ec980",
+    "url": "/zelda-blog/index.html"
+  },
+  {
+    "revision": "2c5f69c4df94ca7d3072",
+    "url": "/zelda-blog/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "73553d0bed406abd9cfa",
+    "url": "/zelda-blog/static/css/main.6b835975.chunk.css"
+  },
+  {
+    "revision": "2c5f69c4df94ca7d3072",
+    "url": "/zelda-blog/static/js/2.be361556.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/zelda-blog/static/js/2.be361556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73553d0bed406abd9cfa",
+    "url": "/zelda-blog/static/js/main.cf89aac7.chunk.js"
+  },
+  {
+    "revision": "14a393e79f47458b24a2",
+    "url": "/zelda-blog/static/js/runtime-main.f1dc6f03.js"
+  },
+  {
+    "revision": "50eaba6287d40490da3bb6c9229b4d00",
+    "url": "/zelda-blog/static/media/ninjaS.50eaba62.png"
+  }
+]);
